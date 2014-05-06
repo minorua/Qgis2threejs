032f82b485d31b8987db1aa5b4a931757d21ec77
Minoru Akagi <akaginch@gmail.com>
2014-05-06 13:15:21 +0900

    binary STL export

--- js/threejs/exporters/STLExporter.js
+++ js/threejs/exporters/STLExporter.js
@@ -1,9 +1,13 @@
 /**
  * @author kovacsv / http://kovacsv.hu/
+ * @author minorua / http://github.com/minorua
  */
  
 THREE.STLExporter = function () {
 	this.stlContent = '';
+	this.isBinary = false;
+	this.header = '';
+	this.binTriangleList = [];
 };
 
 THREE.STLExporter.prototype = {
@@ -58,26 +62,58 @@ THREE.STLExporter.prototype = {
 		};
 		
 		this.addLineToContent ('endsolid exported');
-		return this.stlContent;
+
+		if (this.isBinary) {
+			var buf = new ArrayBuffer(84 + 50 * this.binTriangleList.length);
+			var ui8 = new Uint8Array(buf, 0, 80);
+			for (var i = 0; i < this.header.length; i++) {
+				ui8[i] = this.header.charCodeAt(i);
+			}
+			var numTriangles = new Uint32Array(buf, 80, 1);
+			numTriangles[0] = this.binTriangleList.length;
+			for (var i = 0; i < this.binTriangleList.length; i++) {
+				var src = new Uint8Array(this.binTriangleList[i]);
+				var dst = new Uint8Array(buf, 84 + 50 * i, 50);
+				dst.set(src)
+			}
+			return buf;
+		} else {
+			return this.stlContent;
+		}
 	},
 	
 	clearContent : function ()
 	{
 		this.stlContent = '';
+		this.binTriangleList = []
 	},
 	
 	addLineToContent : function (line) {
-		this.stlContent += line + '\n';
+		if (!this.isBinary) this.stlContent += line + '\n';
 	},
 	
 	addTriangleToContent : function (normal, vertex1, vertex2, vertex3) {
-		this.addLineToContent ('\tfacet normal ' + normal.x + ' ' + normal.y + ' ' + normal.z);
-		this.addLineToContent ('\t\touter loop');
-		this.addLineToContent ('\t\t\tvertex ' + vertex1.x + ' ' + vertex1.y + ' ' + vertex1.z);
-		this.addLineToContent ('\t\t\tvertex ' + vertex2.x + ' ' + vertex2.y + ' ' + vertex2.z);
-		this.addLineToContent ('\t\t\tvertex ' + vertex3.x + ' ' + vertex3.y + ' ' + vertex3.z);
-		this.addLineToContent ('\t\tendloop');
-		this.addLineToContent ('\tendfacet');
+		if (this.isBinary) {
+			var buf = new ArrayBuffer(50);
+			var f32 = new Float32Array(buf, 0, 3 * 4);
+			var i16 = new Uint16Array(buf, 48, 1);
+			var lst = [normal, vertex1, vertex2, vertex3];
+			for (var i = 0; i < 4; i++) {
+				f32[i * 3] = lst[i].x;
+				f32[i * 3 + 1] = lst[i].y;
+				f32[i * 3 + 2] = lst[i].z;
+			}
+			i16[0] = 0;
+			this.binTriangleList.push(buf);
+		} else {
+			this.addLineToContent ('\tfacet normal ' + normal.x + ' ' + normal.y + ' ' + normal.z);
+			this.addLineToContent ('\t\touter loop');
+			this.addLineToContent ('\t\t\tvertex ' + vertex1.x + ' ' + vertex1.y + ' ' + vertex1.z);
+			this.addLineToContent ('\t\t\tvertex ' + vertex2.x + ' ' + vertex2.y + ' ' + vertex2.z);
+			this.addLineToContent ('\t\t\tvertex ' + vertex3.x + ' ' + vertex3.y + ' ' + vertex3.z);
+			this.addLineToContent ('\t\tendloop');
+			this.addLineToContent ('\tendfacet');
+		}
 	},
 	
 	getTransformedPosition : function (vertex, matrix, position) {
