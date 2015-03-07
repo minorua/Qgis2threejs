bceba274d3d8cfb7df0c85c5d570bc280c2a4f78
Minoru Akagi <akaginch@gmail.com>
2015-03-07 10:15:32 +0900

    quad tree: use the word 'rect' instead of 'extent'

--- quadtree.py
+++ quadtree.py
@@ -22,9 +22,9 @@
 from qgis.core import QgsPoint, QgsRectangle
 
 class QuadNode:
-  def __init__(self, parent, extent, location, height=0):
+  def __init__(self, parent, rect, location, height=0):
     self.parent = parent
-    self.extent = extent
+    self.rect = rect
     self.location = location
     self.height = height
     self.subNodes = []
@@ -36,10 +36,10 @@ class QuadNode:
     self.subNodes = []
     for y in range(2):
       for x in range(2):
-        xmin = self.extent.xMinimum() + 0.5 * x * self.extent.width()
-        ymin = self.extent.yMinimum() + 0.5 * (1 - y) * self.extent.height()
-        xmax = xmin + 0.5 * self.extent.width()
-        ymax = ymin + 0.5 * self.extent.height()
+        xmin = self.rect.xMinimum() + 0.5 * x * self.rect.width()
+        ymin = self.rect.yMinimum() + 0.5 * (1 - y) * self.rect.height()
+        xmax = xmin + 0.5 * self.rect.width()
+        ymax = ymin + 0.5 * self.rect.height()
         quadrect = QgsRectangle(xmin, ymin, xmax, ymax)
         node = QuadNode(self, quadrect, 2 * y + x, self.height + 1)
         self.subNodes.append(node)
@@ -55,12 +55,12 @@ class QuadNode:
     return quadlist
 
   def quadByPosition(self, point):
-    if not self.extent.contains(point):
+    if not self.rect.contains(point):
       return None
     if len(self.subNodes) == 0:
       return self
-    x = min(1, int(2 * (point.x() - self.extent.xMinimum()) / self.extent.width()))
-    y = min(1, int(2 * (self.extent.yMaximum() - point.y()) / self.extent.height()))
+    x = min(1, int(2 * (point.x() - self.rect.xMinimum()) / self.rect.width()))
+    y = min(1, int(2 * (self.rect.yMaximum() - point.y()) / self.rect.height()))
     return self.subNodes[2 * y + x].quadByPosition(point)
 
 class QuadTree:
@@ -69,16 +69,15 @@ class QuadTree:
   RIGHT = 2
   DOWN = 3
 
-  def __init__(self, extent=None):
-    self.extent = extent
-    self.root = QuadNode(self, self.extent, 0)
+  def __init__(self, rect=None):
+    if rect is None:
+      rect = QgsRectangle(0, 0, 1, 1)
+    self.rect = rect
+    self.root = QuadNode(self, rect, 0)
     self.focusRect = None
 
-  def setExtent(self, extent):
-    self.extent = extent
-
   def buildTreeByRect(self, rect, height):
-    if not self.extent.intersects(rect):
+    if not self.rect.intersects(rect):
       return
     self.focusRect = QgsRectangle(rect)
     self.height = height
@@ -91,67 +90,67 @@ class QuadTree:
     return self.root.listTopQuads([])
 
   def quadByPosition(self, point):
-    if not point or not self.extent.contains(point):
-      return None
-    return self.root.quadByPosition(point)
+    if point and self.rect.contains(point):
+      return self.root.quadByPosition(point)
+    return None
 
   def neighbors(self, quad):
     # if neighbor count of one direction is not only one, returns one of neighbors. so totally returns 4 neighbors.
     quads = [None] * 4
     if len(quad.parent.subNodes) == 0:
       return quads
-    extent = quad.extent
-    center = extent.center()
+    rect = quad.rect
+    center = rect.center()
     m = 0.5 ** self.height
-    quads[self.UP] = self.quadByPosition(QgsPoint(center.x(), extent.yMaximum() + m * extent.height()))
-    quads[self.LEFT] = self.quadByPosition(QgsPoint(extent.xMinimum() - m * extent.width(), center.y()))
-    quads[self.RIGHT] = self.quadByPosition(QgsPoint(extent.xMaximum() + m * extent.width(), center.y()))
-    quads[self.DOWN] = self.quadByPosition(QgsPoint(center.x(), extent.yMinimum() - m * extent.height()))
+    quads[self.UP] = self.quadByPosition(QgsPoint(center.x(), rect.yMaximum() + m * rect.height()))
+    quads[self.LEFT] = self.quadByPosition(QgsPoint(rect.xMinimum() - m * rect.width(), center.y()))
+    quads[self.RIGHT] = self.quadByPosition(QgsPoint(rect.xMaximum() + m * rect.width(), center.y()))
+    quads[self.DOWN] = self.quadByPosition(QgsPoint(center.x(), rect.yMinimum() - m * rect.height()))
     return quads
 
 class QuadList:
   def __init__(self):
     self.quads = []
-    self.calculatedExtent = None
+    self.calculatedRect = None
     self.sorted = False
 
   def addQuad(self, quad):
     self.quads.append(quad)
-    self.calculatedExtent = None
+    self.calculatedRect = None
     self.sorted = False
 
   def count(self):
     return len(self.quads)
 
-  def extent(self):
-    if self.calculatedExtent is not None:
-      return self.calculatedExtent
+  def rect(self):
+    if self.calculatedRect:
+      return self.calculatedRect
     if len(self.quads) == 0:
       return QgsRectangle()
-    extent = QgsRectangle(self.quads[0].extent)
+    rect = QgsRectangle(self.quads[0].rect)
     for quad in self.quads[1:]:
-      extent.unionRect(quad.extent)
-    self.calculatedExtent = extent
-    return extent
+      rect.unionRect(quad.rect)
+    self.calculatedRect = rect
+    return rect
 
   def width(self):
     if len(self.quads) == 0:
       return 0
-    return int(self.extent().width() / self.quads[0].extent.width() + 0.1)
+    return int(self.rect().width() / self.quads[0].rect.width() + 0.1)
 
   def height(self):
     if len(self.quads) == 0:
       return 0
-    return int(self.extent().height() / self.quads[0].extent.height() + 0.1)
+    return int(self.rect().height() / self.quads[0].rect.height() + 0.1)
 
   def sort(self):
     if self.sorted:
       return
-    extent = self.extent()
+    rect = self.rect()
     sorted_quads = [None] * self.width() * self.height()
     for quad in self.quads:
-      x = int((quad.extent.xMinimum() - extent.xMinimum()) / quad.extent.width() + 0.1)
-      y = int((extent.yMaximum() - quad.extent.yMaximum()) / quad.extent.height() + 0.1)
+      x = int((quad.rect.xMinimum() - rect.xMinimum()) / quad.rect.width() + 0.1)
+      y = int((rect.yMaximum() - quad.rect.yMaximum()) / quad.rect.height() + 0.1)
       sorted_quads[x + y * self.width()] = quad
     self.quads = sorted_quads
     self.sorted = True
