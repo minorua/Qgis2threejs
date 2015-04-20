27c312f5604d530ddd10d4ec6b4e44315222a4ee
Minoru Akagi <akaginch@gmail.com>
2015-04-20 17:11:50 +0900

    bump up js version to 1.3-dev

--- js/Qgis2threejs.js
+++ js/Qgis2threejs.js
@@ -4,7 +4,7 @@
 // (C) 2014 Minoru Akagi | MIT License
 // https://github.com/minorua/Qgis2threejs
 
-var Q3D = {VERSION: "1.2"};
+var Q3D = {VERSION: "1.3-dev"};
 Q3D.Options = {
   bgcolor: null,
   light: {
