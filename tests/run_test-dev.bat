6c80817699790caf7c87889f7a9bc11989dbcc5d
Minoru Akagi <akaginch@gmail.com>
2015-09-07 10:05:44 +0900

    add run_test.bat

--- tests/run_test-dev.bat
+++ tests/run_test-dev.bat
@@ -3,12 +3,4 @@ rem run_test-dev.bat
 rem begin: 2015-09-06
 
 set QGIS_PACKAGE=qgis-dev
-
-call C:\OSGeo4W\bin\o4w_env.bat
-set PATH=%OSGEO4W_ROOT%\apps\%QGIS_PACKAGE%\bin;%PATH%
-set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGIS_PACKAGE%
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGIS_PACKAGE%\python
-
-python run_test.py
-
-pause
+call run_test.bat
