55c438fb5fb5ec28c47962b38f1c7b0238f69f23
Minoru Akagi <akaginch@gmail.com>
2015-09-08 09:55:52 +0900

    add run_test-20.bat (run test with QGIS Dufour)

---
+++ tests/run_test-20.bat
@@ -0,0 +1,7 @@
+@echo off
+rem run_test-dev.bat
+rem begin: 2015-09-06
+
+set OSGEO4W_ROOT=C:\Program Files\QGIS Dufour
+set QGIS_PACKAGE=qgis
+call run_test.bat
--- tests/run_test.bat
+++ tests/run_test.bat
@@ -2,9 +2,10 @@
 rem run_test.bat
 rem begin: 2015-09-06
 
+if "%OSGEO4W_ROOT%" == "" set OSGEO4W_ROOT=C:\OSGeo4W
 if "%QGIS_PACKAGE%" == "" set QGIS_PACKAGE=qgis
 
-call C:\OSGeo4W\bin\o4w_env.bat
+call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 set PATH=%OSGEO4W_ROOT%\apps\%QGIS_PACKAGE%\bin;%PATH%
 set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/%QGIS_PACKAGE%
 set PYTHONPATH=%OSGEO4W_ROOT%\apps\%QGIS_PACKAGE%\python
