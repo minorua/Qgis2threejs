9e950aaa49c13a9c72ebeb2d12ff35097e948d03
Minoru Akagi <akaginch@gmail.com>
2015-09-18 15:48:27 +0900

    sphinx: add a souce parser for markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,10 +37,16 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Add a source parser for markdown
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
