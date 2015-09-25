c5e3e596748e74989c0f9812b04ea4ea383033a8
Minoru Akagi <akaginch@gmail.com>
2015-09-25 13:36:58 +0900

    docs: change local html theme to pyramid

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -136,10 +136,8 @@ rst_epilog = '.. include:: ' + include_filename
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:
+    html_theme = 'pyramid'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
