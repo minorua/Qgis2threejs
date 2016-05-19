192519089587d0dbe5078fe8c89e3c3a0b27367c
Minoru Akagi <akaginch@gmail.com>
2016-05-19 10:57:56 +0900

    update ui/*.py (pyqt4->pyqt5)

--- ui/layerselectdialog.py
+++ ui/layerselectdialog.py
@@ -2,67 +2,53 @@
 
 # Form implementation generated from reading ui file 'D:\Users\minorua\.qgis2\python\developing_plugins\Qgis2threejs\ui\layerselectdialog.ui'
 #
-# Created: Fri Sep 18 10:25:04 2015
-#      by: PyQt4 UI code generator 4.10.2
+# Created by: PyQt5 UI code generator 5.5
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_LayerSelectDialog(object):
     def setupUi(self, LayerSelectDialog):
-        LayerSelectDialog.setObjectName(_fromUtf8("LayerSelectDialog"))
+        LayerSelectDialog.setObjectName("LayerSelectDialog")
         LayerSelectDialog.resize(439, 425)
-        self.verticalLayout = QtGui.QVBoxLayout(LayerSelectDialog)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.tabWidget = QtGui.QTabWidget(LayerSelectDialog)
-        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
-        self.tab_Tree = QtGui.QWidget()
-        self.tab_Tree.setObjectName(_fromUtf8("tab_Tree"))
-        self.gridLayout_2 = QtGui.QGridLayout(self.tab_Tree)
-        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.verticalLayout = QtWidgets.QVBoxLayout(LayerSelectDialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tabWidget = QtWidgets.QTabWidget(LayerSelectDialog)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab_Tree = QtWidgets.QWidget()
+        self.tab_Tree.setObjectName("tab_Tree")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.tab_Tree)
+        self.gridLayout_2.setObjectName("gridLayout_2")
         self.treeView = QgsLayerTreeView(self.tab_Tree)
-        self.treeView.setObjectName(_fromUtf8("treeView"))
+        self.treeView.setObjectName("treeView")
         self.gridLayout_2.addWidget(self.treeView, 0, 0, 1, 1)
-        self.tabWidget.addTab(self.tab_Tree, _fromUtf8(""))
-        self.tab_Preview = QtGui.QWidget()
-        self.tab_Preview.setObjectName(_fromUtf8("tab_Preview"))
-        self.gridLayout = QtGui.QGridLayout(self.tab_Preview)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.tabWidget.addTab(self.tab_Tree, "")
+        self.tab_Preview = QtWidgets.QWidget()
+        self.tab_Preview.setObjectName("tab_Preview")
+        self.gridLayout = QtWidgets.QGridLayout(self.tab_Preview)
+        self.gridLayout.setObjectName("gridLayout")
         self.canvas = QgsMapCanvas(self.tab_Preview)
-        self.canvas.setObjectName(_fromUtf8("canvas"))
+        self.canvas.setObjectName("canvas")
         self.gridLayout.addWidget(self.canvas, 0, 0, 1, 1)
-        self.tabWidget.addTab(self.tab_Preview, _fromUtf8(""))
+        self.tabWidget.addTab(self.tab_Preview, "")
         self.verticalLayout.addWidget(self.tabWidget)
-        self.buttonBox = QtGui.QDialogButtonBox(LayerSelectDialog)
+        self.buttonBox = QtWidgets.QDialogButtonBox(LayerSelectDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
         self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(LayerSelectDialog)
         self.tabWidget.setCurrentIndex(0)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), LayerSelectDialog.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), LayerSelectDialog.reject)
+        self.buttonBox.accepted.connect(LayerSelectDialog.accept)
+        self.buttonBox.rejected.connect(LayerSelectDialog.reject)
         QtCore.QMetaObject.connectSlotsByName(LayerSelectDialog)
 
     def retranslateUi(self, LayerSelectDialog):
-        LayerSelectDialog.setWindowTitle(_translate("LayerSelectDialog", "Select layer(s)", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_Tree), _translate("LayerSelectDialog", "Layers", None))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_Preview), _translate("LayerSelectDialog", "Preview", None))
+        _translate = QtCore.QCoreApplication.translate
+        LayerSelectDialog.setWindowTitle(_translate("LayerSelectDialog", "Select layer(s)"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_Tree), _translate("LayerSelectDialog", "Layers"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_Preview), _translate("LayerSelectDialog", "Preview"))
 
 from qgis.gui import QgsLayerTreeView, QgsMapCanvas
--- ui/settingsdialog.py
+++ ui/settingsdialog.py
@@ -2,86 +2,72 @@
 
 # Form implementation generated from reading ui file 'D:\Users\minorua\.qgis2\python\developing_plugins\Qgis2threejs\ui\settingsdialog.ui'
 #
-# Created: Fri Sep 18 10:25:12 2015
-#      by: PyQt4 UI code generator 4.10.2
+# Created by: PyQt5 UI code generator 5.5
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_SettingsDialog(object):
     def setupUi(self, SettingsDialog):
-        SettingsDialog.setObjectName(_fromUtf8("SettingsDialog"))
+        SettingsDialog.setObjectName("SettingsDialog")
         SettingsDialog.resize(475, 389)
-        self.gridLayout = QtGui.QGridLayout(SettingsDialog)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.buttonBox = QtGui.QDialogButtonBox(SettingsDialog)
+        self.gridLayout = QtWidgets.QGridLayout(SettingsDialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.buttonBox = QtWidgets.QDialogButtonBox(SettingsDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
         self.gridLayout.addWidget(self.buttonBox, 4, 0, 1, 1)
-        self.groupBox_2 = QtGui.QGroupBox(SettingsDialog)
-        self.groupBox_2.setObjectName(_fromUtf8("groupBox_2"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.groupBox_2)
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.tableWidget_Plugins = QtGui.QTableWidget(self.groupBox_2)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        self.groupBox_2 = QtWidgets.QGroupBox(SettingsDialog)
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox_2)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tableWidget_Plugins = QtWidgets.QTableWidget(self.groupBox_2)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.tableWidget_Plugins.sizePolicy().hasHeightForWidth())
         self.tableWidget_Plugins.setSizePolicy(sizePolicy)
         self.tableWidget_Plugins.setMaximumSize(QtCore.QSize(16777215, 100))
-        self.tableWidget_Plugins.setObjectName(_fromUtf8("tableWidget_Plugins"))
+        self.tableWidget_Plugins.setObjectName("tableWidget_Plugins")
         self.tableWidget_Plugins.setColumnCount(0)
         self.tableWidget_Plugins.setRowCount(0)
         self.verticalLayout.addWidget(self.tableWidget_Plugins)
-        self.label_2 = QtGui.QLabel(self.groupBox_2)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_2 = QtWidgets.QLabel(self.groupBox_2)
+        self.label_2.setObjectName("label_2")
         self.verticalLayout.addWidget(self.label_2)
-        self.textBrowser_Plugin = QtGui.QTextBrowser(self.groupBox_2)
+        self.textBrowser_Plugin = QtWidgets.QTextBrowser(self.groupBox_2)
         self.textBrowser_Plugin.setOpenExternalLinks(True)
-        self.textBrowser_Plugin.setObjectName(_fromUtf8("textBrowser_Plugin"))
+        self.textBrowser_Plugin.setObjectName("textBrowser_Plugin")
         self.verticalLayout.addWidget(self.textBrowser_Plugin)
         self.gridLayout.addWidget(self.groupBox_2, 1, 0, 1, 1)
-        self.groupBox = QtGui.QGroupBox(SettingsDialog)
-        self.groupBox.setObjectName(_fromUtf8("groupBox"))
-        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.groupBox)
-        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
-        self.label = QtGui.QLabel(self.groupBox)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.groupBox = QtWidgets.QGroupBox(SettingsDialog)
+        self.groupBox.setObjectName("groupBox")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.groupBox)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setObjectName("label")
         self.horizontalLayout_2.addWidget(self.label)
-        self.lineEdit_BrowserPath = QtGui.QLineEdit(self.groupBox)
-        self.lineEdit_BrowserPath.setObjectName(_fromUtf8("lineEdit_BrowserPath"))
+        self.lineEdit_BrowserPath = QtWidgets.QLineEdit(self.groupBox)
+        self.lineEdit_BrowserPath.setObjectName("lineEdit_BrowserPath")
         self.horizontalLayout_2.addWidget(self.lineEdit_BrowserPath)
-        self.toolButton_Browse = QtGui.QToolButton(self.groupBox)
-        self.toolButton_Browse.setObjectName(_fromUtf8("toolButton_Browse"))
+        self.toolButton_Browse = QtWidgets.QToolButton(self.groupBox)
+        self.toolButton_Browse.setObjectName("toolButton_Browse")
         self.horizontalLayout_2.addWidget(self.toolButton_Browse)
         self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
 
         self.retranslateUi(SettingsDialog)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), SettingsDialog.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), SettingsDialog.reject)
+        self.buttonBox.accepted.connect(SettingsDialog.accept)
+        self.buttonBox.rejected.connect(SettingsDialog.reject)
         QtCore.QMetaObject.connectSlotsByName(SettingsDialog)
 
     def retranslateUi(self, SettingsDialog):
-        SettingsDialog.setWindowTitle(_translate("SettingsDialog", "Settings", None))
-        self.groupBox_2.setTitle(_translate("SettingsDialog", "Optional Features (Plugins)", None))
-        self.label_2.setText(_translate("SettingsDialog", "Description", None))
-        self.groupBox.setTitle(_translate("SettingsDialog", "General", None))
-        self.label.setText(_translate("SettingsDialog", "Browser path", None))
-        self.toolButton_Browse.setText(_translate("SettingsDialog", "Browse", None))
+        _translate = QtCore.QCoreApplication.translate
+        SettingsDialog.setWindowTitle(_translate("SettingsDialog", "Settings"))
+        self.groupBox_2.setTitle(_translate("SettingsDialog", "Optional Features (Plugins)"))
+        self.label_2.setText(_translate("SettingsDialog", "Description"))
+        self.groupBox.setTitle(_translate("SettingsDialog", "General"))
+        self.label.setText(_translate("SettingsDialog", "Browser path"))
+        self.toolButton_Browse.setText(_translate("SettingsDialog", "Browse"))
 
