73364df22776f26460f9275ab99b8428e41ba362
Minoru Akagi <akaginch@gmail.com>
2017-06-09 09:53:48 +0900

    [three.js] updating three.js to r82 solved material disposal problem

--- js/Qgis2threejs.js
+++ js/Qgis2threejs.js
@@ -55,7 +55,7 @@ custom function
 
 Q3D.Scene = function () {
   THREE.Scene.call(this);
-  this.autoUpdate = false;    // .matrixAutoUpdate (in recent three.js version)
+  this.autoUpdate = false;
 
   this.mapLayers = {};
 
@@ -1351,9 +1351,7 @@ Q3D.MapLayer.prototype.removeAllObjects = function () {
   var i, l;
   for (i = 0, l = this.materials.length; i < l; i++) {
     if (this.materials[i].map) this.materials[i].map.dispose();   // dispose of texture
-    // this.materials[i].m.dispose();
-    //TODO: error occurs during next rendering if materials are disposed here (with three.js r70).
-    //      updating three.js solves this problem?
+    this.materials[i].m.dispose();
   }
   this.materials = [];
 
