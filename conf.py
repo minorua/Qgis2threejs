c69c2deeb93cb73a23df79189481c0d58bac43b2
Minoru Akagi <akaginch@gmail.com>
2018-04-12 11:50:25 +0900

    version 2.0

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@
  ***************************************************************************/
 """
 
-plugin_version = "1.99.0"
+plugin_version = "2.0"
 debug_mode = 1
   # 0. no debug info
   # 1. JS console, qDebug
