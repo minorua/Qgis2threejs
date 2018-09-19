0903f3eb6a0bfb6d062fc0de5bdde992bfd0e736
Minoru Akagi <akaginch@gmail.com>
2018-09-19 14:44:46 +0900

    DeviceOrientationControls: device.alpha can be zero or null

--- js/threejs/controls/DeviceOrientationControls.js
+++ js/threejs/controls/DeviceOrientationControls.js
@@ -85,7 +85,7 @@ THREE.DeviceOrientationControls = function( object ) {
 
 		if ( device ) {
 
-			var alpha = device.alpha ? THREE.Math.degToRad( device.alpha ) + scope.alphaOffset : 0; // Z
+			var alpha = (device.alpha !== null) ? THREE.Math.degToRad( device.alpha ) + scope.alphaOffset : 0; // Z
 
 			var beta = device.beta ? THREE.Math.degToRad( device.beta ) : 0; // X'
 
