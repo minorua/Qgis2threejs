10e4f3de44fffd64a8b3defae50e2438f5862eaf
jonnyforestGIS <joao.f.r.gaspar@gmail.com>
2019-07-24 14:29:57 +0100

    [Fix] Change depecreated class

--- js/mobile.js
+++ js/mobile.js
@@ -261,7 +261,7 @@ function startARMode(position) {
     });
 
     var id = devices[0].deviceId;
-    navigator.getUserMedia({video: {optional: [{sourceId: id}]}}, function (stream) {
+    navigator.mediaDevices.getUserMedia({video: {optional: [{sourceId: id}]}}, function (stream) {
       var v = document.getElementById("video");
       v.addEventListener("loadedmetadata", function () {
         app.eventListener.resize();
