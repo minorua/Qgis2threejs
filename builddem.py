dda13b3bf8e4fc1844595799f1c1e433009ba7db
Minoru Akagi <akaginch@gmail.com>
2021-02-12 10:38:37 +0900

    [DEM] fix edge processing

--- builddem.py
+++ builddem.py
@@ -29,6 +29,7 @@ from .datamanager import MaterialManager
 from .buildlayer import LayerBuilder
 from .geometry import VectorGeometry, LineGeometry, TINGeometry, dissolvePolygonsWithinExtent
 from .mapextent import MapExtent
+from .qgis2threejstools import logMessage
 
 
 class DEMLayerBuilder(LayerBuilder):
@@ -108,6 +109,7 @@ class DEMLayerBuilder(LayerBuilder):
         size = self.properties.get("spinBox_Size", 1) if surroundings else 1
         size2 = size * size
 
+        center_block = None
         blks = []
         for i in range(size2):
             sx = i % size - (size - 1) // 2
@@ -143,6 +145,16 @@ class DEMLayerBuilder(LayerBuilder):
                                     clip_geometry=clip_geometry if is_center else None,
                                     pathRoot=self.pathRoot,
                                     urlRoot=self.urlRoot)
+            if is_center:
+                block.roughness = 1
+
+                center_block = block
+            else:
+                block.roughness = roughness
+
+                if sx * sx <= 1 and sy * sy <= 1:
+                    block.neighbors.append((sx, sy, center_block, 1))
+
             yield block
 
 
@@ -167,6 +179,10 @@ class DEMBlockBuilder:
         self.clip_geometry = clip_geometry
         self.pathRoot = pathRoot
         self.urlRoot = urlRoot
+
+        self.roughness = 1
+        self.neighbors = []
+        self.edges = None
 
     def build(self):
         mapTo3d = self.settings.mapTo3d()
@@ -196,7 +212,7 @@ class DEMBlockBuilder:
         else:
             grid_width, grid_height = (self.grid_seg.width() + 1, self.grid_seg.height() + 1)
 
-            if self.edgeRoughness == 1:
+            if self.edgeRoughness == 1 and len(self.neighbors) == 0:
                 ba = self.provider.read(grid_width, grid_height, self.extent)
             else:
                 grid_values = list(self.provider.readValues(grid_width, grid_height, self.extent))
@@ -293,42 +309,121 @@ class DEMBlockBuilder:
         return d
 
     def processEdges(self, grid_values, roughness):
-        grid_width = self.grid_seg.width() + 1
-        grid_height = self.grid_seg.height() + 1
-        rg_grid_width = self.grid_seg.width() // roughness + 1
-        rg_grid_height = self.grid_seg.height() // roughness + 1
+
+        if self.offsetX == 0 and self.offsetY == 0:
+            self.processEdgesCenter(grid_values, roughness)
+            return
+
+        grid_width, grid_height = (self.grid_seg.width() + 1,
+                                   self.grid_seg.height() + 1)
+
+        for sx, sy, neighbor, roughness in self.neighbors:
+            if self.roughness <= roughness:
+                continue
+            if neighbor.edges is None:
+                logMessage("Neighbor block {} holds no edge values.".format(neighbor.blockIndex))
+                continue
+
+            if (sx, sy) == (0, -1):
+                # top edge
+                for x in range(grid_width):
+                    grid_values[x] = neighbor.edges[0][x]
+
+            elif (sx, sy) == (0, 1):
+                # bottom edge
+                offset = grid_width * (grid_height - 1)
+                for x in range(grid_width):
+                    grid_values[offset + x] = neighbor.edges[3][x]
+
+            elif (sx, sy) == (-1, 0):
+                # right edge
+                offset = grid_width - 1
+                for y in range(grid_height):
+                    grid_values[offset + grid_width * y] = neighbor.edges[1][y]
+
+            elif (sx, sy) == (1, 0):
+                # left edge
+                for y in range(grid_height):
+                    grid_values[grid_width * y] = neighbor.edges[2][y]
+
+            elif (sx, sy) == (-1, -1):
+                # top-right corner
+                grid_values[grid_width - 1] = neighbor.edges[0][0]
+
+            elif (sx, sy) == (1, -1):
+                # top-left corner
+                grid_values[0] = neighbor.edges[0][grid_width - 1]
+
+            elif (sx, sy) == (-1, 1):
+                # bottom-right corner
+                grid_values[grid_width * grid_height - 1] = neighbor.edges[3][0]
+
+            elif (sx, sy) == (1, 1):
+                # bottom-left corner
+                grid_values[grid_width * (grid_height - 1)] = neighbor.edges[3][grid_width - 1]
+
+            else:
+                logMessage("Edge processing: invalid sx and sy ({}, {})".format(sx, sy))
+
+
+    def processEdgesCenter(self, grid_values, roughness):
+
+        grid_width, grid_height = (self.grid_seg.width() + 1,
+                                   self.grid_seg.height() + 1)
+        rg_grid_width, rg_grid_height = (self.grid_seg.width() // roughness + 1,
+                                         self.grid_seg.height() // roughness + 1)
         ii = range(roughness)[1:]
+
+        iy0 = grid_width * (grid_height - 1)
+        e_top = [grid_values[0]]
+        e_bottom = [grid_values[iy0]]
 
         for x0 in range(rg_grid_width - 1):
             # top edge
             ix0 = x0 * roughness
             z0 = grid_values[ix0]
             z1 = grid_values[ix0 + roughness]
+            s = (z1 - z0) / roughness
             for i in ii:
-                grid_values[ix0 + i] = (z1 - z0) * i / roughness + z0
+                grid_values[ix0 + i] = z0 + s * i
+
+            e_top.append(z1)
 
             # bottom edge
-            iy0 = grid_width * (grid_height - 1)
             z0 = grid_values[iy0 + ix0]
             z1 = grid_values[iy0 + ix0 + roughness]
+            s = (z1 - z0) / roughness
             for i in ii:
-                grid_values[iy0 + ix0 + i] = (z1 - z0) * i / roughness + z0
+                grid_values[iy0 + ix0 + i] = z0 + s * i
+
+            e_bottom.append(z1)
+
+        e_left = [grid_values[0]]
+        e_right = [grid_values[grid_width - 1]]
 
         rw = roughness * grid_width
         for y0 in range(rg_grid_height - 1):
             # left edge
-            iy0 = y0 * grid_width
+            iy0 = y0 * rw
             z0 = grid_values[iy0]
             z1 = grid_values[iy0 + rw]
+            s = (z1 - z0) / roughness
             for i in ii:
-                grid_values[iy0 + i * grid_width] = (z1 - z0) * i / roughness + z0
+                grid_values[iy0 + i * grid_width] = z0 + s * i
+
+            e_left.append(z1)
 
             # right edge
             iy0 += grid_width - 1
             z0 = grid_values[iy0]
             z1 = grid_values[iy0 + rw]
+            s = (z1 - z0) / roughness
             for i in ii:
-                grid_values[iy0 + i * grid_width] = (z1 - z0) * i / roughness + z0
+                grid_values[iy0 + i * grid_width] = z0 + s * i
+
+            e_right.append(z1)
+
+        self.edges = [e_bottom, e_left, e_right, e_top]
 
     def getValue(self, x, y):
 
