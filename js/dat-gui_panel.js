abb9ded4149b7289c2b68ba11402c6c860f875c7
Minoru Akagi <akaginch@gmail.com>
2022-02-14 17:09:31 +0900

    dat-gui: small fixes and IIFE

--- js/dat-gui_panel.js
+++ js/dat-gui_panel.js
@@ -11,6 +11,7 @@ Q3D.gui.dat = {
   type: "dat-gui",
 
   parameters: {
+
     lyr: {},
     cp: {
       c: "#ffffff",
@@ -22,41 +23,62 @@ Q3D.gui.dat = {
       rot: false,  // auto rotation
       wf: false    // wireframe mode
     },
-    i: Q3D.application.showInfo
-  },
+    i: Q3D.gui.showInfo
+  }
+};
+
+(function () {
+
+  var app = Q3D.application,
+      _this = Q3D.gui.dat;
+
+  Q3D.gui.modules.push(_this);
+
+  var panel;
 
   // initialize gui
   // - setupDefaultItems: default is true
   // - params: parameter values to pass to dat.GUI constructor
-  init: function (setupDefaultItems, params) {
+  _this.init = function (setupDefaultItems, params) {
     setupDefaultItems = (setupDefaultItems === undefined) ? true : setupDefaultItems;
 
-    this.gui = new dat.GUI(params);
-    this.gui.domElement.parentElement.style.zIndex = 2000;   // display the panel on the front of labels
+    panel = new dat.GUI(params);
+    panel.domElement.parentElement.style.zIndex = 2000;   // display the panel on the front of labels
+
+    this.gui = panel;
 
     if (setupDefaultItems) {
-      this.layersFolder = this.gui.addFolder('Layers');
-      if (Q3D.Config.gui.customPlane) this.customPlaneFolder = this.gui.addFolder('Custom Plane');
+      this.layersFolder = panel.addFolder('Layers');
+      if (Q3D.Config.gui.customPlane) this.customPlaneFolder = panel.addFolder('Custom Plane');
       if (Q3D.Config.animation.enabled) this.addAnimationFolder();
       if (Q3D.isTouchDevice) this.addCommandsFolder();
       this.addHelpButton();
     }
-  },
+  };
 
-  initLayersFolder: function (scene) {
+  _this.initLayersFolder = function (scene) {
     var mapLayers = scene.mapLayers;
-    var parameters = this.parameters;
-    var visibleChanged = function (value) { mapLayers[this.object.i].visible = value; };
-    var opacityChanged = function (o) { mapLayers[this.object.i].opacity = o; };
-    var mtlChanged = function (idx) { mapLayers[this.object.i].setCurrentMaterial(idx); };
+    var params = this.parameters;
+
+    var visibleChanged = function (value) {
+      mapLayers[this.object.i].visible = value;
+    };
+
+    var opacityChanged = function (o) {
+      mapLayers[this.object.i].opacity = o;
+    };
+
+    var mtlChanged = function (idx) {
+      mapLayers[this.object.i].setCurrentMaterial(idx);
+    };
 
     var layer, folder, mtlNames, i, items;
     for (var layerId in mapLayers) {
       layer = mapLayers[layerId];
-      parameters.lyr[layerId] = {i: layerId, v: layer.visible, o: layer.opacity, m: 0};
+      params.lyr[layerId] = {i: layerId, v: layer.visible, o: layer.opacity, m: 0};
       folder = this.layersFolder.addFolder(layer.properties.name);
-      folder.add(parameters.lyr[layerId], 'v').name('Visible').onChange(visibleChanged);
-      folder.add(parameters.lyr[layerId], 'o').min(0).max(1).name('Opacity').onChange(opacityChanged);
+      folder.add(params.lyr[layerId], 'v').name('Visible').onChange(visibleChanged);
+      folder.add(params.lyr[layerId], 'o').min(0).max(1).name('Opacity').onChange(opacityChanged);
 
       mtlNames = layer.properties.mtlNames;
       if (mtlNames && mtlNames.length > 1) {
@@ -64,25 +86,22 @@ Q3D.gui.dat = {
         for (i = 0; i < mtlNames.length; i++) {
           items[mtlNames[i]] = i;
         }
-        folder.add(parameters.lyr[layerId], 'm', items).name('Material').onChange(mtlChanged);
+        folder.add(params.lyr[layerId], 'm', items).name('Material').onChange(mtlChanged);
       }
     }
     return this.layersFolder;
-  },
+  };
 
-  customPlaneMaterial: function (color) {
+  _this.customPlaneMaterial = function (color) {
     var m = new THREE.MeshLambertMaterial({color: color, transparent: true})
     if (!Q3D.isIE) m.side = THREE.DoubleSide;
     return m;
-  },
+  };
 
-  initCustomPlaneFolder: function (zMin, zMax) {
-    var app = Q3D.application,
-        gui = Q3D.gui.dat;
-
+  _this.initCustomPlaneFolder = function (zMin, zMax) {
     var scene = app.scene,
         p = scene.userData,
-        parameters = gui.parameters;
+        params = this.parameters;
 
     if (zMin === undefined || zMax === undefined) {
       var box = new THREE.Box3().setFromObject(scene);
@@ -93,50 +112,49 @@ Q3D.gui.dat = {
     var addPlane = function (color) {
       // Add a new plane in the current scene
       var geometry = new THREE.PlaneBufferGeometry(p.baseExtent.width, p.baseExtent.height, 1, 1),
-          material = gui.customPlaneMaterial(color);
-      gui.customPlane = new THREE.Mesh(geometry, material);
-      gui.customPlane.rotation.z = p.baseExtent.rotation * Q3D.deg2rad;
-      scene.add(gui.customPlane);
+          material = _this.customPlaneMaterial(color);
+      _this.customPlane = new THREE.Mesh(geometry, material);
+      _this.customPlane.rotation.z = p.baseExtent.rotation * Q3D.deg2rad;
+      scene.add(_this.customPlane);
       app.render();
     };
-    parameters.cp.d = zMin;
+    params.cp.d = zMin;
 
     // Plane color
-    this.customPlaneFolder.addColor(parameters.cp, 'c').name('Color').onChange(function (value) {
-      if (gui.customPlane === undefined) addPlane(parameters.cp.c);
-      gui.customPlane.material.color.setStyle(value);
+    this.customPlaneFolder.addColor(params.cp, 'c').name('Color').onChange(function (value) {
+      if (_this.customPlane === undefined) addPlane(params.cp.c);
+      _this.customPlane.material.color.setStyle(value);
       app.render();
     });
 
     // Plane altitude
-    this.customPlaneFolder.add(parameters.cp, 'd').min(zMin).max(zMax).name('Altitude').onChange(function (value) {
-      if (gui.customPlane === undefined) addPlane(parameters.cp.c);
-      gui.customPlane.position.z = (value + p.zShift) * p.zScale;
-      gui.customPlane.updateMatrixWorld();
+    this.customPlaneFolder.add(params.cp, 'd').min(zMin).max(zMax).name('Altitude').onChange(function (value) {
+      if (_this.customPlane === undefined) addPlane(params.cp.c);
+      _this.customPlane.position.z = (value + p.zShift) * p.zScale;
+      _this.customPlane.updateMatrixWorld();
       app.render();
     });
 
     // Plane opacity
-    this.customPlaneFolder.add(parameters.cp, 'o').min(0).max(1).name('Opacity (0-1)').onChange(function (value) {
-      if (gui.customPlane === undefined) addPlane(parameters.cp.c);
-      gui.customPlane.material.opacity = value;
+    this.customPlaneFolder.add(params.cp, 'o').min(0).max(1).name('Opacity (0-1)').onChange(function (value) {
+      if (_this.customPlane === undefined) addPlane(params.cp.c);
+      _this.customPlane.material.opacity = value;
       app.render();
     });
 
     // Enlarge plane option
-    this.customPlaneFolder.add(parameters.cp, 'l').name('Enlarge').onChange(function (value) {
-      if (gui.customPlane === undefined) addPlane(parameters.cp.c);
-      if (value) gui.customPlane.scale.set(80, 80, 1);
-      else gui.customPlane.scale.set(1, 1, 1);
-      gui.customPlane.updateMatrixWorld();
+    this.customPlaneFolder.add(params.cp, 'l').name('Enlarge').onChange(function (value) {
+      if (_this.customPlane === undefined) addPlane(params.cp.c);
+      if (value) _this.customPlane.scale.set(80, 80, 1);
+      else _this.customPlane.scale.set(1, 1, 1);
+      _this.customPlane.updateMatrixWorld();
       app.render();
     });
-  },
+  };
 
-  addAnimationFolder: function () {
-    var app = Q3D.application,
-        anim = app.animation.keyframes;
-    var btn, folder = this.gui.addFolder('Animation');
+  _this.addAnimationFolder = function () {
+    var anim = app.animation.keyframes;
+    var btn, folder = panel.addFolder('Animation');
 
     this.parameters.anm = {
       p: function () {
@@ -144,29 +162,33 @@ Q3D.gui.dat = {
           anim.pause();
           btn.name('Resume');
         }
-        else {
-          if (anim.isPaused) anim.resume();
-          else anim.start();
+        else if (anim.isPaused) {
+          anim.resume();
           btn.name('Pause');
+        }
+        else {
+          anim.start();
         }
     }};
     btn = folder.add(this.parameters.anm, 'p').name('Play');
 
+    app.addEventListener('animationStarted', function () {
+      btn.name('Pause');
+    });
+
     app.addEventListener('animationStopped', function () {
       btn.name('Play');
     });
-  },
+  };
 
   // add commands folder for touch screen devices
-  addCommandsFolder: function () {
-    var folder = this.gui.addFolder('Commands');
-    folder.add(this.parameters.cmd, 'rot').name('Orbit Animation').onChange(Q3D.application.setRotateAnimationMode);
-    folder.add(this.parameters.cmd, 'wf').name('Wireframe Mode').onChange(Q3D.application.setWireframeMode);
-  },
+  _this.addCommandsFolder = function () {
+    var folder = panel.addFolder('Commands');
+    folder.add(this.parameters.cmd, 'rot').name('Orbit Animation').onChange(app.setRotateAnimationMode);
+    folder.add(this.parameters.cmd, 'wf').name('Wireframe Mode').onChange(app.setWireframeMode);
+  };
 
-  addHelpButton: function () {
-    this.gui.add(this.parameters, 'i').name('Help');
-  }
-};
-
-Q3D.gui.modules.push(Q3D.gui.dat);
+  _this.addHelpButton = function () {
+    panel.add(this.parameters, 'i').name('Help');
+  };
+})();
