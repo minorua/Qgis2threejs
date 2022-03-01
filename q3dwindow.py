affd482b4d33d8d33d9b77e9df2882ab39bb0b39
Minoru Akagi <akaginch@gmail.com>
2022-03-01 21:30:31 +0900

    update point cloud layer icon (for QGIS < 3.18)

--- q3dwindow.py
+++ q3dwindow.py
@@ -172,7 +172,7 @@ class Q3DWindow(QMainWindow):
             LayerType.POINT: QgsApplication.getThemeIcon("mIconPointLayer.svg"),
             LayerType.LINESTRING: QgsApplication.getThemeIcon("mIconLineLayer.svg"),
             LayerType.POLYGON: QgsApplication.getThemeIcon("mIconPolygonLayer.svg"),
-            LayerType.POINTCLOUD: QgsApplication.getThemeIcon("mIconPointCloudLayer.svg") if Qgis.QGIS_VERSION_INT >= 31800 else QIcon(pluginDir("images", "pointcloud.svg"))
+            LayerType.POINTCLOUD: QgsApplication.getThemeIcon("mIconPointCloudLayer.svg") if Qgis.QGIS_VERSION_INT >= 31800 else QIcon(pluginDir("svg", "pointcloud.svg"))
         }
 
     def setupMenu(self):
