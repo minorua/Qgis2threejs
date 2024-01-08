332ee20b3bea14ba172755827ee0cd51059f6f9e
Minoru Akagi <akaginch@gmail.com>
2024-01-08 11:11:59 +0900

    sphinx: set rst_epilog

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,8 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# Replacements for images
+rst_epilog = '.. include:: replace_images.txt'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
