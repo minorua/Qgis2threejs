1a083e015e25b8a4608decf2ffbd482f903d1da9
Minoru Akagi <akaginch@gmail.com>
2025-04-29 16:22:14 +0900

    pyuic6.bat: replace PyQt6 with qgis.PyQt

--- scripts/pyuic6.bat
+++ scripts/pyuic6.bat
@@ -1,10 +1,11 @@
 @echo off
 call C:\OSGeo4W\bin\o4w_env.bat
-path C:\OSGeo4W\apps\qt6\bin;%PATH%
+path C:\OSGeo4W\apps\qt6\bin;%PATH%;"C:\Program Files\Git\usr\bin"
 
 cd /d %~dp1
 
 @echo on
 pyuic6.exe -o %~n1.py %~n1.ui
 
+sed -i "s/from PyQt6 import/from qgis.PyQt import/g" %~n1.py
 pause
