c00cb6186e5e66e145408c19e5f6df3704e27479
Minoru Akagi <akaginch@gmail.com>
2026-02-28 17:35:14 +0900

    add randomly generated point cloud dataset for testproject2
    - add pointcloud.las generated using a Python script created by ChatGPT
    - add Entwine Point Tile (EPT) files generated by OSGeo4W QGIS 3.22
    - add Potree files generated with Potree Converter 1.6

---
+++ tests/data/testproject2/potree16/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1000,
+    "boundingBox": {
+        "lx": 300.46,
+        "ly": 300.32,
+        "lz": 0.0,
+        "ux": 400.08,
+        "uy": 399.94,
+        "uz": 99.62
+    },
+    "tightBoundingBox": {
+        "lx": 300.46,
+        "ly": 300.32,
+        "lz": 0.0,
+        "ux": 399.97,
+        "uy": 399.94,
+        "uz": 49.89
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.8627344965934753,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
