minorua/Qgis2threejs

commit afec2438789c091226e55ba229b459cf0cf4cc7f

Separate Browser Process: Q3DWebViewProxy and Q3DWebPageProxy classes

core/controller/controller.py
@@ -215,7 +215,7 @@ class Q3DController(QObject):
             return
 
         if not self.enabled:
-            self.webPage.runScript("setPreviewEnabled(false)")      # bypass controller enablement check
+            self.webPage.setPreviewEnabled(False)
             self.clearStatusMessage()
             return
 

gui/webenginecommon.py
@@ -1,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# (C) 2026 Minoru Akagi
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import json
+
+# This module may be used in an external process rather than within the QGIS process.
+try:
+    from PyQt6.QtCore import QEventLoop, QTimer
+    from PyQt6.QtGui import QDesktopServices
+    from PyQt6.QtWebEngineCore import QWebEnginePage
+except ImportError:
+    from PyQt5.QtCore import QEventLoop, QTimer
+    from PyQt5.QtGui import QDesktopServices
+    from PyQt5.QtWebEngineWidgets import QWebEnginePage
+
+from .webviewcommon import Q3DWebPageCommon, Q3DWebViewCommon, TIMEOUT_MS, WEBVIEWTYPE_WEBENGINE
+from ..utils import logger
+
+
+class Q3DWebEnginePageCommon(Q3DWebPageCommon):
+
+    def runScript(self, string, message="", sourceID="webenginecommon.py", callback=None, wait=False):
+        """
+        Run a JavaScript script in the web view with optional data and callback.
+        Args:
+            string (str): The JavaScript code string to execute.
+            message (str, optional): A descriptive message for logging purposes.
+            sourceID (str, optional): Identifier for the source of the script.
+            callback (optional): Callback function to be executed after script runs.
+            wait (bool, optional): Whether to wait for script execution to complete.
+        """
+        self.logScriptExecution(string, message, sourceID)
+
+        if not wait:
+            if callback:
+                self.runJavaScript(string, callback)
+            else:
+                self.runJavaScript(string)
+            return
+
+        loop = QEventLoop()
+        timer = QTimer()
+        timer.setSingleShot(True)
+        timer.timeout.connect(loop.quit)
+
+        finished = False
+        result = None
+        def runJavaScriptCallback(res):
+            nonlocal finished, result
+            finished = True
+            result = res
+            loop.quit()
+
+        self.runJavaScript(string, runJavaScriptCallback)
+
+        timer.start(TIMEOUT_MS)
+        loop.exec()
+
+        if not finished:
+            logger.warning(f"JavaScript execution timed out: {string}")
+
+        if callback:
+            callback(result)
+
+        return result
+
+    def logToConsole(self, message, level="debug"):
+        if level not in ["debug", "info", "warn", "error"]:
+            level = "log"
+
+        if level in ["warn", "error"]:
+            self.jsErrorWarning.emit(bool(level == "error"))
+
+        msg = json.dumps(message.replace('\n', '\\n'))      # new line causes issues in console
+
+        self.runJavaScript(f"console.{level}({msg});")
+
+    def acceptNavigationRequest(self, url, type, isMainFrame):
+        if type == QWebEnginePage.NavigationType.NavigationTypeLinkClicked:
+            QDesktopServices.openUrl(url)
+            return False
+        return True
+
+    def requestRendering(self, waitUntilFinished=False):
+        def render():
+            self.runScript("requestRendering()")
+
+        if waitUntilFinished:
+            loop = QEventLoop()
+            self.bridge.requestedRenderingFinished.connect(loop.quit)
+
+            timer = QTimer()
+            timer.setSingleShot(True)
+            timer.timeout.connect(loop.quit)
+
+            render()
+
+            timer.start(TIMEOUT_MS)
+            loop.exec()
+        else:
+            render()
+
+
+class Q3DWebEngineViewCommon(Q3DWebViewCommon):
+
+    def __init__(self, _=None):
+        super().__init__()
+
+        self.webViewType = WEBVIEWTYPE_WEBENGINE

gui/webengineview.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # begin: 2023-10-03
 
-import json
 import os
 import logging
 
@@ -24,7 +23,7 @@ except ImportError:
 
 from .webview_conf import DEBUG_MODE
 from .webview_utils import pluginDir, logger, web_logger
-from .webviewcommon import Q3DWebPageCommon, Q3DWebViewCommon, WEBVIEWTYPE_WEBENGINE
+from .webenginecommon import Q3DWebEnginePageCommon, Q3DWebEngineViewCommon
 
 
 TIMEOUT_MS = 30000      # timeout (ms) for script execution and rendering
@@ -64,13 +63,13 @@ def restoreChromiumFlags():
             del os.environ[KEY]
 
 
-class Q3DWebEnginePage(Q3DWebPageCommon, QWebEnginePage):
+class Q3DWebEnginePage(Q3DWebEnginePageCommon, QWebEnginePage):
 
     jsErrorWarning = pyqtSignal(bool)       # bool: is_error
 
     def __init__(self, parent=None):
         QWebEnginePage.__init__(self, parent)
-        Q3DWebPageCommon.__init__(self)
+        Q3DWebEnginePageCommon.__init__(self)
 
         self.isWebEnginePage = True
 
@@ -90,84 +89,9 @@ class Q3DWebEnginePage(Q3DWebPageCommon, QWebEnginePage):
         self.showStatusMessage("Initializing preview...")
         self.setUrl(self.myUrl)
 
-    def runScript(self, string, message="", sourceID="webengineview.py", callback=None, wait=False):
-        """
-        Run a JavaScript script in the web view with optional data and callback.
-        Args:
-            string (str): The JavaScript code string to execute.
-            message (str, optional): A descriptive message for logging purposes.
-            sourceID (str, optional): Identifier for the source of the script.
-            callback (optional): Callback function to be executed after script runs.
-            wait (bool, optional): Whether to wait for script execution to complete.
-        """
-        self.logScriptExecution(string, message, sourceID)
-
-        if not wait:
-            if callback:
-                self.runJavaScript(string, callback)
-            else:
-                self.runJavaScript(string)
-            return
-
-        loop = QEventLoop()
-        timer = QTimer()
-        timer.setSingleShot(True)
-        timer.timeout.connect(loop.quit)
-
-        finished = False
-        result = None
-        def runJavaScriptCallback(res):
-            nonlocal finished, result
-            finished = True
-            result = res
-            loop.quit()
-
-        self.runJavaScript(string, runJavaScriptCallback)
-
-        timer.start(TIMEOUT_MS)
-        loop.exec()
-
-        if not finished:
-            logger.warning(f"JavaScript execution timed out: {string}")
-
-        if callback:
-            callback(result)
-
-        return result
-
     def sendData(self, data, viaQueue=False):
         logger.debug("Sending {} data to web page...".format(data.get("type", "unknown")))
         self.bridge.sendData.emit(data, viaQueue)
-
-    def requestRendering(self, waitUntilFinished=False):
-        def render():
-            self.runScript("requestRendering()")
-
-        if waitUntilFinished:
-            loop = QEventLoop()
-            self.bridge.requestedRenderingFinished.connect(loop.quit)
-
-            timer = QTimer()
-            timer.setSingleShot(True)
-            timer.timeout.connect(loop.quit)
-
-            render()
-
-            timer.start(TIMEOUT_MS)
-            loop.exec()
-        else:
-            render()
-
-    def logToConsole(self, message, level="debug"):
-        if level not in ["debug", "info", "warn", "error"]:
-            level = "log"
-
-        if level in ["warn", "error"]:
-            self.jsErrorWarning.emit(bool(level == "error"))
-
-        msg = json.dumps(message.replace('\n', '\\n'))      # new line causes issues in console
-
-        self.runJavaScript(f"console.{level}({msg});")
 
     def javaScriptConsoleMessage(self, level, message, lineNumber, sourceID):
         CML = QWebEnginePage.JavaScriptConsoleMessageLevel
@@ -194,15 +118,17 @@ class Q3DWebEnginePage(Q3DWebPageCommon, QWebEnginePage):
         return True
 
 
-class Q3DWebEngineView(Q3DWebViewCommon, QWebEngineView):
+class Q3DWebEngineView(Q3DWebEngineViewCommon, QWebEngineView):
+
+    WebPageClass = Q3DWebEnginePage
 
     def __init__(self, parent):
         setChromiumFlags()
 
         QWebEngineView.__init__(self, parent)
-        Q3DWebViewCommon.__init__(self)
+        Q3DWebEngineViewCommon.__init__(self)
 
-        self._page = Q3DWebEnginePage(self)
+        self._page = self.WebPageClass(self)
         self._page.setObjectName("webEnginePage")
         self.setPage(self._page)
 

gui/webkitview.py
@@ -13,7 +13,7 @@ from qgis.PyQt.QtWidgets import QDialog, QVBoxLayout
 from qgis.PyQt.QtWebKit import QWebSettings, QWebSecurityOrigin
 from qgis.PyQt.QtWebKitWidgets import QWebInspector, QWebPage, QWebView
 
-from .webviewcommon import Q3DWebPageCommon, Q3DWebViewCommon
+from .webviewcommon import Q3DWebPageCommon, Q3DWebViewCommon, WEBVIEWTYPE_WEBKIT
 from ..conf import DEBUG_MODE
 from ..utils import js_bool, pluginDir
 from ..utils.logging import logger, web_logger
@@ -45,6 +45,8 @@ class Q3DWebKitPage(Q3DWebPageCommon, QWebPage):
         return self.mainFrame().url()
 
     def setup(self):
+        Q3DWebPageCommon.setup(self)
+
         # if self.offScreen:
         #     # transparent background
         #     palette = self.palette()
@@ -112,6 +114,7 @@ class Q3DWebKitView(Q3DWebViewCommon, QWebView):
     def __init__(self, parent):
         QWebView.__init__(self, parent)
         Q3DWebViewCommon.__init__(self)
+        self.webViewType = WEBVIEWTYPE_WEBKIT
 
         self._page = Q3DWebKitPage(self)
         self._page.setObjectName("webKitPage")

gui/webview.py
@@ -58,8 +58,12 @@ def getWebViewClass(webViewType=None, webViewMode=None):
         return Q3DWebKitView
 
     if webViewType == WEBVIEWTYPE_WEBENGINE:
-        from .webengineview import Q3DWebEngineView
-        return Q3DWebEngineView
+        if webViewMode == WVM_INPROCESS:
+            from .webengineview import Q3DWebEngineView
+            return Q3DWebEngineView
+
+        from .webviewproxy import Q3DWebViewProxy
+        return Q3DWebViewProxy
 
     from .fallbackview import Q3DFallbackView
     return Q3DFallbackView
@@ -71,14 +75,18 @@ def getWebPageClass(webViewType=WEBVIEWTYPE_WEBENGINE, webViewMode=WVM_INPROCESS
         return Q3DWebKitPage
 
     if webViewType == WEBVIEWTYPE_WEBENGINE:
-        from .webengineview import Q3DWebEnginePage
-        return Q3DWebEnginePage
+        if webViewMode == WVM_INPROCESS:
+            from .webengineview import Q3DWebEnginePage
+            return Q3DWebEnginePage
+
+        from .webviewproxy import Q3DWebPageProxy
+        return Q3DWebPageProxy
 
     from .fallbackview import Q3DFallbackPage
     return Q3DFallbackPage
 
 
-def setDefaultWebView(webViewType):
+def setDefaultWebView(webViewType, webViewMode=WVM_INPROCESS):
     global Q3DView, Q3DWebPage, defaultWebViewType
 
     if webViewType is defaultWebViewType:
@@ -90,9 +98,14 @@ def setDefaultWebView(webViewType):
         Q3DWebPage = Q3DWebKitPage
 
     elif webViewType == WEBVIEWTYPE_WEBENGINE:
-        from .webengineview import Q3DWebEngineView, Q3DWebEnginePage
-        Q3DView = Q3DWebEngineView
-        Q3DWebPage = Q3DWebEnginePage
+        if webViewMode == WVM_INPROCESS:
+            from .webengineview import Q3DWebEngineView, Q3DWebEnginePage
+            Q3DView = Q3DWebEngineView
+            Q3DWebPage = Q3DWebEnginePage
+        else:
+            from.webviewproxy import Q3DWebViewProxy, Q3DWebPageProxy
+            Q3DView = Q3DWebViewProxy
+            Q3DWebPage = Q3DWebPageProxy
 
     else:
         from .fallbackview import Q3DFallbackView, Q3DFallbackPage

gui/webviewcommon.py
@@ -45,7 +45,7 @@ class Q3DWebPageCommon:
     def teardown(self):
         pass
 
-    def pageLoaded(self):
+    def pageLoaded(self, _ok):
         self.loadedScripts = {}
 
     def scriptFileLoaded(self, scriptFileId):
@@ -54,16 +54,6 @@ class Q3DWebPageCommon:
         callbacks = self.loadScriptCallbacks.pop(scriptFileId, [])
         for cb in callbacks:
             cb()
-
-    def logScriptExecution(self, string, message="", sourceID=""):
-        if not DEBUG_MODE or message is None:
-            return
-
-        text = message or string
-        if sourceID:
-            text += f"\t({sourceID})"
-
-        logger.debug(f"> {text}")
 
     def loadScriptFile(self, scriptFileId, callback=None, wait=False):
         if scriptFileId in self.loadedScripts:
@@ -95,9 +85,6 @@ class Q3DWebPageCommon:
             self.runScript(script)
 
     def loadScriptFiles(self, scriptFileIds, callback=None):
-        if not scriptFileIds:
-            raise Exception("loadScriptFiles called with empty scriptFileIds")
-
         remaining = list(scriptFileIds)
 
         def load_next():
@@ -110,6 +97,16 @@ class Q3DWebPageCommon:
             self.loadScriptFile(id, callback=load_next)
 
         load_next()
+
+    def logScriptExecution(self, string, message="", sourceID=""):
+        if not DEBUG_MODE or message is None:
+            return
+
+        text = message or string
+        if sourceID:
+            text += f"\t({sourceID})"
+
+        logger.debug(f"> {text}")
 
     def showMessageBar(self, msg, timeout_ms=0, warning=False):
         """Show a message bar at the top of the web page.
@@ -132,12 +129,12 @@ class Q3DWebViewCommon:
     def __init__(self, _=None):
         self.setAcceptDrops(True)
 
-    def setup(self, enabled=True):
+    def setup(self, webViewMode=None, enabledAtStart=True):
         """
+        :param webViewMode: in-process, embedded external, external window
         :param enabled: whether preview is enabled at start
         """
-        self._enabled = enabled     # whether preview is enabled at start
-
+        self._enabled = enabledAtStart
         self._page.setup()
 
     def teardown(self):
@@ -159,3 +156,9 @@ class Q3DWebViewCommon:
             QMessageBox.information(self, "three.js Renderer Info", str(info))
 
         self.runScript("app.renderer.info", callback=showInfo)
+
+    def setPreviewEnabled(self, enabled):
+        if enabled:
+            self._page.reload()
+        else:
+            self.runScript("setPreviewEnabled(false)")

gui/webviewproxy.py
@@ -1,0 +1,351 @@
+# -*- coding: utf-8 -*-
+# (C) 2026 Minoru Akagi
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import json
+import os
+import subprocess
+
+from qgis.PyQt.QtCore import Qt, QObject, QTimer, QUrl, pyqtSignal
+from qgis.PyQt.QtGui import QPalette, QPixmap, QWindow
+from qgis.PyQt.QtWidgets import QPushButton, QLabel, QStackedLayout, QVBoxLayout, QWidget
+
+from .webbridge import WebIPCBridge
+from .webenginecommon import Q3DWebEnginePageCommon, Q3DWebEngineViewCommon
+from .webview import WVM_EXTERNAL_WINDOW
+from ..conf import DEBUG_MODE
+from ..preview.ipc_const import Event, Request
+from ..preview.socketserver import SocketServer
+from ..utils import createUid, pluginDir
+from ..utils.logging import logger, web_logger
+
+TIMEOUT_MS = 30000      # timeout (ms) for script execution and rendering
+
+
+class Q3DWebPageProxy(Q3DWebEnginePageCommon, QObject):
+
+    BridgeClass = WebIPCBridge
+
+    jsErrorWarning = pyqtSignal(bool)       # is_error
+
+    # QWebEnginePage signals
+    loadStarted = pyqtSignal()
+    loadFinished = pyqtSignal(bool)
+
+    def __init__(self, parent=None):
+        QObject.__init__(self, parent)
+        Q3DWebEnginePageCommon.__init__(self)
+
+        self.isWebEnginePage = True
+
+        url = pluginDir("web/viewer/webengine.html").replace("\\", "/")
+        self.myUrl = QUrl.fromLocalFile(url)
+
+    def setSocketServer(self, server):
+        self.socketServer = server
+        self.socketServer.notified.connect(self.notified)
+        self.socketServer.requestReceived.connect(self.requestReceived)
+        self.socketServer.responseReceived.connect(self.responseReceived)
+
+    def notified(self, method, params, payload):
+        if method == Event.METHOD_INVOKED:
+            func = getattr(self.bridge, params["name"])
+            func(*params["args"])
+
+        elif method == Event.PAGE_LOAD_STARTED:
+            self.loadStarted.emit()
+
+        elif method == Event.PAGE_LOADED:
+            self.loadFinished.emit(params.get("ok", False))
+
+        elif method == Event.JS_ERROR_WARNING:
+            self.jsErrorWarning.emit(params.get("is_error", False))
+
+    def requestReceived(self, id, method, params, payload):
+        pass
+
+    def responseReceived(self, id, method, params, payload):
+        pass
+
+    def reload(self):
+        self.showStatusMessage("Initializing preview in an external process...")
+        self.socketServer.request(Request.RELOAD)
+
+    def url(self):
+        return self.myUrl
+
+    def runJavaScript(self, string, callback=None):
+        self.socketServer.request(Request.RUN_SCRIPT, params={"script": string}, callback=callback)
+
+    def sendData(self, data, viaQueue=False):
+        logger.debug("Sending {} data to web page...".format(data.get("type", "unknown")))
+
+        params = {
+            "data": data,
+            "viaQueue": viaQueue
+        }
+        self.socketServer.request(Request.LOAD_DATA, params=params)
+
+        return
+
+        # use shared memory
+        b = json.dumps(data).encode("utf-8")
+        size = len(b)
+        logger.debug(f"Data size: {size:,}")
+
+        params = {"viaQueue": viaQueue}
+        if size < 4000:
+            params["data"] = data
+            payload = None
+        else:
+            payload = b
+
+        self.socketServer.request(Request.LOAD_DATA, params=params, payload=payload)
+
+
+class Q3DWebViewProxy(Q3DWebEngineViewCommon, QWidget):
+
+    # TODO: fileDropped - IPC
+
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
+        Q3DWebEngineViewCommon.__init__(self, parent)
+
+        self.embeddedMode = True
+        self.viewProcess = None
+        self.previewWnd = None
+
+        self.serverName = "Q3D" + createUid()
+        self.socketServer = SocketServer(self, self.serverName)
+        self.socketServer.requestReceived.connect(self.requestReceived)
+        self.socketServer.disconnected.connect(self.disconnected)
+
+        self._page = Q3DWebPageProxy(self)
+        self._page.setSocketServer(self.socketServer)
+
+        self.previewStateWidget = PreviewStateWidget(self)
+        self.previewStateWidget.buttonRestart.clicked.connect(self.startPreview)
+
+        self.stackedLayout = QStackedLayout(self)
+        self.stackedLayout.addWidget(self.previewStateWidget)
+
+    def page(self):
+        return self._page
+
+    def setup(self, webViewMode=None, enabledAtStart=True):
+        Q3DWebEngineViewCommon.setup(self, webViewMode, enabledAtStart)
+
+        if webViewMode == WVM_EXTERNAL_WINDOW:
+            self.embeddedMode = False
+
+        self.setPreviewEnabled(enabledAtStart)
+
+    def teardown(self):
+        logger.info("Socket server is going to shut down.")
+        self.stopPreview()
+
+    def startPreview(self):
+        self.stackedLayout.setCurrentIndex(0)
+
+        if self.viewProcess:
+            self.stopPreview()
+
+        logger.info("Launching preview...")
+        self.previewStateWidget.setState(PreviewStateWidget.State_Loading)
+
+        args = []
+        if os.name == "nt":
+            if DEBUG_MODE:
+                args += [
+                    pluginDir("scripts", "pause_on_error.bat"),
+                    "python"
+                ]
+            else:
+                args.append("pythonw")
+
+        if not args:
+            args.append("python3")
+
+        args += [
+            "-m", "Qgis2threejs.preview.view",
+            "-s", self.serverName,
+            "-p", str(os.getpid())
+        ]
+
+        if not self.embeddedMode:
+            args.append("-f")
+
+        cwd = os.path.dirname(pluginDir())
+
+        # env = os.environ.copy()
+        # env["PYTHONPATH"] = os.pathsep.join(sys.path)
+        # del env["QT3D_RENDERER"]
+        # env["QTWEBENGINE_CHROMIUM_FLAGS"] = "--enable-logging --v=1"
+
+        if hasattr(subprocess, "STARTUPINFO"):
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+            if self.embeddedMode:
+                startupinfo.wShowWindow = 6  # SW_MINIMIZE
+            else:
+                startupinfo.wShowWindow = 1  # SW_SHOWNORMAL
+        else:
+            startupinfo = None
+
+        self.viewProcess = subprocess.Popen(args, cwd=cwd, startupinfo=startupinfo)        # env=env
+
+    def stopPreview(self):
+        self.socketServer.notify(Event.QUIT)
+
+        if self.previewWnd:
+            self.previewWnd.hide()
+            self.previewWnd.setParent(None)
+            self.previewWnd = None
+
+        if self.viewProcess:
+            try:
+                self.viewProcess.terminate()
+                self.viewProcess.wait(timeout=3)
+
+            # except ProcessLookupError:
+            # except subprocess.TimeoutExpired:
+            finally:
+                self.viewProcess = None
+
+    def setPreviewEnabled(self, enabled):
+        if enabled:
+            self.startPreview()
+        else:
+            self.previewStateWidget.setState(PreviewStateWidget.State_Disabled)
+            self.stackedLayout.setCurrentIndex(0)
+            self.stopPreview()
+
+    # TODO:
+    def setAcceptDrops(self, _b):
+        pass
+
+    def showDevTools(self):
+        self.socketServer.notify(Event.DEV_TOOLS)
+
+    def showGPUInfo(self):
+        self.socketServer.notify(Event.GPU_INFO)
+
+    def requestReceived(self, id, method, params, payload):
+        if method == Request.EMBED_WND:
+            winId = int(params.get("winId"))
+            self.previewWnd = QWindow.fromWinId(winId)
+            container = QWidget.createWindowContainer(self.previewWnd)
+
+            w = self.stackedLayout.widget(1)
+            if w:
+                w.hide()
+                self.stackedLayout.removeWidget(w)
+
+            self.stackedLayout.addWidget(container)
+            self.stackedLayout.setCurrentIndex(1)
+
+            logger.info(f"External window ({winId}) embedded.")
+            self.previewStateWidget.setState(PreviewStateWidget.State_Idle)
+        else:
+            return
+
+        self.socketServer.respond(id, method)
+
+    def disconnected(self):
+        logger.info("Disconnected from preview process.")
+        if self.previewStateWidget.currentState != PreviewStateWidget.State_Disabled:
+            self.previewStateWidget.setState(PreviewStateWidget.State_Error)
+        self.stackedLayout.setCurrentIndex(0)
+
+
+class PreviewStateWidget(QWidget):
+
+    State_Idle = 0
+    State_Loading = 1
+    State_Error = 2
+    State_Disabled = 3
+
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
+
+        self.setAutoFillBackground(True)
+
+        self.msg1 = QLabel(self)
+        self.msg1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.msg2 = QLabel(self)
+        self.msg2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.buttonRestart = QPushButton(self)
+        self.buttonRestart.setText("RESTART PREVIEW")
+        self.buttonRestart.setStyleSheet("padding: 6px 12px;")
+        self.buttonRestart.hide()
+
+        self.icon = QLabel(self)
+        self.icon.setPixmap(QPixmap(pluginDir("Qgis2threejs.png")))
+        self.icon.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.icon.setDisabled(True)
+        self.icon.hide()
+
+        layout = QVBoxLayout()
+        layout.addStretch(1)
+        layout.addWidget(self.msg1)
+        layout.addWidget(self.msg2)
+        layout.addStretch(1)
+        layout.addWidget(self.buttonRestart, 0, Qt.AlignmentFlag.AlignHCenter)
+        layout.addWidget(self.icon)
+        layout.addStretch(3)
+
+        self.setLayout(layout)
+
+        self.timer = QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.timeout)
+
+        self.currentMsg1 = self.currentMsg2 = ""
+        self.currentState = PreviewStateWidget.State_Idle
+        self.dots = 0
+
+    def setState(self, state):
+        self.timer.stop()
+        self.buttonRestart.hide()
+        self.icon.hide()
+
+        msg1 = msg2 = ""
+        bgcolor = None
+        if state == PreviewStateWidget.State_Loading:
+            msg1 = "PREPARING PREVIEW"
+            bgcolor = Qt.GlobalColor.white
+
+            self.dots = 0
+            self.timer.start()
+
+        elif state == PreviewStateWidget.State_Error:
+            msg1 = "PREVIEW STOPPED UNEXPECTEDLY.\nTHE CONNECTION WAS LOST."
+            self.buttonRestart.show()
+
+        elif state == PreviewStateWidget.State_Disabled:
+            self.icon.show()
+
+        if bgcolor is None:
+            bgcolor = self.palette().color(QPalette.ColorGroup.Disabled, QPalette.ColorRole.Button)
+
+        self.msg1.setText(msg1)
+        self.msg2.setText(msg2)
+        self.setBackgroundColor(bgcolor)
+
+        self.currentState = state
+        self.currentMsg1 = msg1
+        self.currentMsg2 = msg2
+
+    def timeout(self):
+        self.dots = (self.dots + 1) % 4
+
+        dots = "." * self.dots + " " * (3 - self.dots)
+        self.msg1.setText(self.currentMsg1 + " " + dots)
+
+    def setBackgroundColor(self, color):
+        pal = self.palette()
+        pal.setColor(QPalette.ColorRole.Window, color)
+        self.setPalette(pal)

commit 3f0aa4a2b09e14cc3e870c1100435cdd2218bbc4

fix AttributeError caused by afec243

core/controller/controller.py
@@ -220,7 +220,7 @@ class Q3DController(QObject):
             return
 
         if not self.enabled:
-            self.webPage.setPreviewEnabled(False)
+            self.runScript("setPreviewEnabled(false)")
             self.clearStatusMessage()
             return
 