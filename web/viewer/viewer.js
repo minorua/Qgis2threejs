75618f9ef94630285fb3467ffb886b06570f5bec
Minoru Akagi <akaginch@gmail.com>
2026-06-26 09:13:02 +0900

    remove JS polyfills

--- web/viewer/viewer.js
+++ web/viewer/viewer.js
@@ -652,29 +652,3 @@ app.saveCanvasImage = function (width, height, fill_background) {
 	};
 	app._saveCanvasImage(width, height, fill_background, saveCanvasImage);
 };
-
-//// polyfills
-
-if (!Number.isNaN) {
-	Number.isNaN = function (v) {
-		return v !== v;
-	};
-}
-
-// for binary glTF export
-// https://developer.mozilla.org/ja/docs/Web/API/HTMLCanvasElement/toBlob
-if (!HTMLCanvasElement.prototype.toBlob) {
-	Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
-		value: function (callback, type, quality) {
-			var binStr = atob(this.toDataURL(type, quality).split(',')[1]),
-				len = binStr.length,
-				arr = new Uint8Array(len);
-
-			for (var i = 0; i < len; i++) {
-				arr[i] = binStr.charCodeAt(i);
-			}
-
-			callback(new Blob([arr], {type: type || 'image/png'}));
-		}
- 	});
-}
