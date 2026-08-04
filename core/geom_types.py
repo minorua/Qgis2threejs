b31a63883a488c7217d75012f36dc26dbae1b0ea
Minoru Akagi <akaginch@gmail.com>
2026-08-05 07:31:35 +0900

    use TypeAlias

--- core/geom_types.py
+++ core/geom_types.py
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 from collections.abc import Callable
+from typing import TypeAlias
 
-# types
-Vector3 = tuple[float, float, float]
-Triangle = tuple[Vector3, Vector3, Vector3]
-Face = tuple[int, int, int]
+Vector3: TypeAlias = tuple[float, float, float]
+Triangle: TypeAlias = tuple[Vector3, Vector3, Vector3]
+Face: TypeAlias = tuple[int, int, int]
 
-TransformFunc = Callable[[float, float, float], Vector3]
-ZFunc = Callable[[float, float], float]
+TransformFunc: TypeAlias = Callable[[float, float, float], Vector3]
+ZFunc: TypeAlias = Callable[[float, float], float]
