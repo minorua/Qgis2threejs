b52022e1b3affed739f4e014733c28f10221e8bd
Minoru Akagi <akaginch@gmail.com>
2026-08-26 21:10:03 +0900

    add v3.2 source files
    - remove source and build directories for v2.8 and v2.9

--- build_302.bat
+++ build_302.bat
@@ -5,14 +5,6 @@ pushd %~dp0
 
 call .venv\Scripts\activate.bat
 
-set SOURCEDIR=source/2.10
-set BUILDDIR=docs/docs/2.10
-call make.bat html
-
-set SOURCEDIR=source/3.1
-set BUILDDIR=docs/docs/3.1
-call make.bat html
-
 set SOURCEDIR=source/3.2
 set BUILDDIR=docs/docs/3.2
 call make.bat html
@@ -20,3 +12,5 @@ call make.bat html
 call .venv\Scripts\deactivate.bat
 
 popd
+
+pause
