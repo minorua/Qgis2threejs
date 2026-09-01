17c7be1ab4adfb2d66ea4c8d75f001a894fef8e1
Minoru Akagi <akaginch@gmail.com>
2026-09-01 17:52:39 +0900

    add BoundingVolume class

--- core/mapextent.py
+++ core/mapextent.py
@@ -369,3 +369,54 @@ class RegularGrid:
                 row_max - row_min + 1,
             )
         )
+
+
+@dataclass
+class ZRange:
+    zmin: float
+    zmax: float
+
+
+@dataclass
+class BoundingVolume:
+
+    xmin: float
+    ymin: float
+    zmin: float
+    xmax: float
+    ymax: float
+    zmax: float
+
+    def xSize(self):
+        return self.xmax - self.xmin
+
+    def ySize(self):
+        return self.ymax - self.ymin
+
+    def zSize(self):
+        return self.zmax - self.zmin
+
+    def toObbData(self):
+        hx = self.xSize() / 2
+        hy = self.ySize() / 2
+        hz = self.zSize() / 2
+
+        cx = self.xmin + hx
+        cy = self.ymin + hy
+        cz = self.zmin + hz
+
+        return [
+            cx, cy, cz,
+            hx, 0, 0,
+            0, hy, 0,
+            0, 0, hz
+        ]
+
+    @staticmethod
+    def fromExtent(extent: MapExtent, zmin, zmax):
+        rect = extent.unrotatedRect()
+        return BoundingVolume(rect.xMinimum(), rect.yMinimum(), zmin, rect.xMaximum(), rect.yMaximum(), zmax)
+
+    @staticmethod
+    def fromRect(rect: QgsRectangle, zrange: ZRange):
+        return BoundingVolume(rect.xMinimum(), rect.yMinimum(), zrange.zmin, rect.xMaximum(), rect.yMaximum(), zrange.zmax)
